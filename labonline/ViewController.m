81367ce09adefdb10adc7964ffbe823dc8957c8d
cocim01 <cocim01@mini.local>
2015-03-24 10:56:33 +0800

    Initial Commit

---
+++ labonline/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  labonline
+//
+//  Created by cocim01 on 15/3/24.
+//  Copyright (c) 2015年 科希盟. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ labonline/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  labonline
+//
+//  Created by cocim01 on 15/3/24.
+//  Copyright (c) 2015年 科希盟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
